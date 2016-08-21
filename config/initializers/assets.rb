53adad178ce54398320de8b127a28f9bc00d3986
g4-cft <jd.levarato@gmail.com>
2016-08-21 21:01:19 +0200

    font awesome + fixes + opti

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( application.css )
 Rails.application.config.assets.precompile += %w( sd.css )
 Rails.application.config.assets.precompile += %w( hd.css )
+Rails.application.config.assets.precompile += %w( fontawesome.css )
 
 #JS
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
