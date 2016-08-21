f494cd0d29e89ad0058a8613bd30ff797729abd4
Drillan767 <jd.levarato@gmail.com>
2016-08-21 16:47:32 +0200

    Complété les modal des membres staff, modifié l'apparence des pages modal avec l'image de profil en float right

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -20,7 +20,124 @@ $('#brandy').click(function(){
         '<div class="row">'+
         '<div class="col-md-12">'+
         '<div class="flex">' +
-        '<div class="flex-pair text-center"><img src="assets/pair.png" alt="pair" />'+
+        '<div><img src="assets/pair.png" alt="pair" />'+
+        '<img src="assets/impair.png" alt="impair" />'+
+        '</div>' +
+        '</div>'+
+        '</div>'+
+        '</div>');
+    $('#infos').modal('toggle');
+    $('#modal').animate({ scrollTop: 0 }, 'slow');
+});
+
+$('#keops').click(function(){
+    $('.modal-content').empty().append('<div class="modal-header">'+
+        '<h2>Keops - Rédacteur en chef</h2>'+
+        '</div>'+
+        '<div class="modal-body">'+
+        '<div class="row">'+
+        '<div>'+
+        '<img src="/assets/presentation/Keops.png" alt="Keops" class="img-presentation">'+
+        '<p>Enchanté, je suis Keops mais vous me connaissez déjà puisque je suis le rédacteur' +
+        'en chef du webzine depuis ses débuts, et je suis également aspirant scénariste.' +
+        'Fasciné depuis l’enfance par le monde de la BD en général, cette passion ne veut toujours' +
+        'pas me quitter. Alors j’espère que vous suivrez le Kippon Dream encore longtemps et que vous' +
+        'nous permettrez de réaliser encore plus d’objectifs, jusqu’à la consécration' +
+        '</p>' +
+        '</div>'+
+        '</div>'+
+        '<div class="row">'+
+        '<div class="col-md-12">'+
+        '<div class="flex">' +
+        '<div><img src="assets/pair.png" alt="pair" />'+
+        '<img src="assets/impair.png" alt="impair" />'+
+        '</div>' +
+        '</div>'+
+        '</div>'+
+        '</div>');
+    $('#infos').modal('toggle');
+    $('#modal').animate({ scrollTop: 0 }, 'slow');
+});
+
+$('#kimel').click(function(){
+    $('.modal-content').empty().append('<div class="modal-header">'+
+        '<h2>Kimel - Graphiste</h2>'+
+        '</div>'+
+        '<div class="modal-body">'+
+        '<div class="row">'+
+        '<div>'+
+        // '<img src="/assets/presentation/Kimel.png" alt="Kimel" class="img-presentation">'+
+        '<p>Hello ! Je suis Kimel, j ai 20 ans, et je suis actuellement étudiante en web :) ' +
+        'J ai pour passion beaucoup de choses,mais principalement les animes (SAO, DBZ, Kyoukai no Kanata...), ' +
+        ' la Kpop depuis récemment (c est un monde particulier avec que des gens gentils <3) et aussi le graphisme ' +
+        '(depuis 7 ans). Je souhaite devenir développeur web et apprendre à faire de jolis designs de site, parce ' +
+        'que c est trop cool <3 ' +
+        '</p>' +
+        '</div>'+
+        '</div>'+
+        '<div class="row">'+
+        '<div class="col-md-12">'+
+        '<div class="flex">' +
+        '<div><img src="assets/pair.png" alt="pair" />'+
+        '<img src="assets/impair.png" alt="impair" />'+
+        '</div>' +
+        '</div>'+
+        '</div>'+
+        '</div>');
+    $('#infos').modal('toggle');
+    $('#modal').animate({ scrollTop: 0 }, 'slow');
+});
+$('#niko').click(function(){
+    $('.modal-content').empty().append('<div class="modal-header">'+
+        '<h2>Niko - </h2>'+
+        '</div>'+
+        '<div class="modal-body">'+
+        '<div class="row">'+
+        '<div>'+
+        '<img src="/assets/presentation/Niko.png" alt="Niko" class="img-presentation">'+
+        '<p>Salut à tous, moi c est Niko j ai 40 ans et ça fait 40 ans que je suis passionné par le manga' +
+        'et la BD en général... Mes principales sources d inspirations sont des auteurs comme Akira TORIYAMA' +
+        '(DBZ) et Eiichiro ODA (One Piece) et bien d autres… Fier d intégrer une équipe pleine d idées comme le ' +
+        'KIPPON DREAM.  J espère que vous prendrez autant de plaisir à découvrir "les Chroniques des Skull" que' +
+        'moi à les dessiner.... <br /> ' +
+        'Bonne lecture avec le KIPPON DREAM et à bientôt ! ' +
+        '</p>' +
+        '</div>'+
+        '</div>'+
+        '<div class="row">'+
+        '<div class="col-md-12">'+
+        '<div class="flex">' +
+        '<div><img src="assets/pair.png" alt="pair" />'+
+        '<img src="assets/impair.png" alt="impair" />'+
+        '</div>' +
+        '</div>'+
+        '</div>'+
+        '</div>');
+    $('#infos').modal('toggle');
+    $('#modal').animate({ scrollTop: 0 }, 'slow');
+});
+
+$('#twildette').click(function(){
+    $('.modal-content').empty().append('<div class="modal-header">'+
+        '<h2>Twildette - Graphiste </h2>'+
+        '</div>'+
+        '<div class="modal-body">'+
+        '<div class="row">'+
+        '<div>'+
+        '<img src="/assets/presentation/twildette.png" alt="Twildette" class="img-presentation">'+
+        '<p>Bonjooooour :D Twilde, ou Twildette ; je suis Bretonne et j’ai 23 ans ! Je suis la graphiste ' +
+        'principale du Kippon Dream ! J’ai débarqué au numéro 6 sur internet; c’est moi qui m’occupe de la mise ' +
+        'en page des magazines; je me suis également occupé de la partie graphique du site internet. Je suis tombée ' +
+        'dans l’univers de la BD et du manga grâce à Yumi, il y a plus de 10 ans : on achetait un tome sur deux de Shaman ' +
+        'King et je repassais et mettais en couleur ses dessins et ses planches. ' +
+        'Ça nous rajeunit pas, mais je suis heureuse de voir à quel point on a progressé !' +
+        '</p>' +
+        '</div>'+
+        '</div>'+
+        '<div class="row">'+
+        '<div class="col-md-12">'+
+        '<div class="flex">' +
+        '<div><img src="assets/pair.png" alt="pair" />'+
         '<img src="assets/impair.png" alt="impair" />'+
         '</div>' +
         '</div>'+
@@ -39,6 +156,7 @@ $('#ataru').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/ataru.png" alt="Ataru" class="img-presentation">'+
         '<p>Je suis Ataru, amoureux du dessin, de la BD, de l’animation et du jeu vidéo. Au risque de passer pour un ancien, ' +
         ' mes maîtres sont Tezuka, Toriyama, Takahashi Rumiko et d’autres auteurs plus proches comme Franquin ou Gotlib.' +
         ' J’ai fait du dessin animé il y a quelques années avec Monsieur René Borg (un projet assez avancé est malheureusement en attente,' +
@@ -58,8 +176,7 @@ $('#ataru').click(function(){
         '<p>O.S.E.F</p>'+
         '<p class="liste-serie"> #15 - One-Shot </p>'+
         '</div>'+
-        '<div class="col-md-6">'+
-        '<img src="/assets/presentation/ataru.png" alt="Ataru" class="img-presentation">'+
+        '<div class="col-md-6">' +
         '</div>'+
         '</div>'+
         '</div>'+
@@ -75,6 +192,7 @@ $('#ayu').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/ayu.png" alt="Ayu" class="img-presentation">'+
         '<p>Passionnée de dessin depuis mon enfance, j’essaie depuis le collège de créer mes propres mangas.' +
         'Je continue depuis plusieurs années déjà de dessiner mon projet' +
         'principal Animix. Mes inspirations sont Walt Disney et Obata Takeshi. En dehors du dessin, je suis passionnée' +
@@ -95,7 +213,6 @@ $('#ayu').click(function(){
         '#14 - Chapitre 6 </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/ayu.png" alt="Ayu" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -111,6 +228,7 @@ $('#christ').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Christ.png" alt="Christ" class="img-presentation">'+
         '<p>' +
         '</p>'+
         '</div>'+
@@ -126,8 +244,7 @@ $('#christ').click(function(){
         '<p class="liste-serie"> #8 - One Shot <br/> #10 - Chapitre 1 <br/> #12 - Chapitre 2 <br/>' +
         '#14 - Chapitre 3 </p>'+
         '</div>'+
-        '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Christ.png" alt="Christ" class="img-presentation">'+
+        '<div class="col-md-6">' +
         '</div>'+
         '</div>'+
         '</div>'+
@@ -143,6 +260,7 @@ $('#emu').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Emu.png" alt="Emueiichi" class="img-presentation">'+
         '<p>Hellow ! C’est EmuEiichi ! J’ai 17 ans, et je dessine depuis plusieurs années, mais je verse une petite larme' +
         'de fragile quand je vois que mon évolution et un peu lente... TTvTT J’ai toujours adoré le dynamisme et la liberté' +
         'que possède le manga, que possède peu (ou presque pas) la BD franco-belge. Et comme beaucoup, j’ai été influencé' +
@@ -165,7 +283,6 @@ $('#emu').click(function(){
         '#13 - Chapitre 3 <br/> #15 - Chapitre 4 </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Emu.png" alt="Emueiichi" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -181,6 +298,7 @@ $('#julien').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Julien.png" alt="Julien Mestre" class="img-presentation">'+
         '<p> Moi c’est Julien! Je fais partie de la génération Club Dorothée et j’aime énormément Dragon Ball, Saint Seiya,' +
         'Captain Tsubasa… J’aime aussi Spider-Man et les comics Marvel. En ce moment je travaille sur les BD' +
         'Dawn of the Sorain et  Paranormal Investigation. Ah oui, j’adore les jeux vidéo, le saucisson et le poulet !' +
@@ -201,7 +319,6 @@ $('#julien').click(function(){
         '<p class="liste-serie"> #15 One Shot</p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Julien.png" alt="Julien Mestre" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -217,6 +334,7 @@ $('#kowasu').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Kowasu.png" alt="Kowasu" class="img-presentation">'+
         '<p> Bonjour , je suis Kowasu , l’auteur de No-Life Makoto , je suis accessoirement une illustratrice pour le KD' +
         'J’ai pas grand chose à dire sur moi à part vous souhaiter une agréable lecture !</p>'+
         '</div>'+
@@ -235,7 +353,6 @@ $('#kowasu').click(function(){
         '#4 - Chapitre 4 <br/> #5 Chapitre - 5 <br/> #6 - Chapitre 6 <br/> #7 - Chapitre 7 <br/ #8 - Chapitre 8</p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Kowasu.png" alt="Kowasu" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -251,6 +368,7 @@ $('#kuro').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Kuro.png" alt="Kuro" class="img-presentation">'+
         '<p>Hellow, moi c’est Kuro, je suis le scénariste de Dawn of the Sorain  et le webmaster du site internet que vous parcourez en ce moment!' +
         'Ecrire a toujours été ma  passion et quand j’ai découvert les mangas j’ai tout de suite eu envie d’en créer un.' +
         'One Piece, Death Note, Steins Gate (bon d’accord, c’est un light-novel et un anime, mais je le case quand même car il' +
@@ -271,7 +389,6 @@ $('#kuro').click(function(){
         '#11 - Chapitre 3 <br/> #13 - Chapitre 4 </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Kuro.png" alt="Kuro" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -287,6 +404,7 @@ $('#larryon').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Larryon.png" alt="Larryon" class="img-presentation">'+
         '<p>Je suis Larryon, bientôt 25 ans, habite en région parisienne et dessinateur de la série Dragon(s).'+
         'J’ai intégré le Kippon Dream au numéro 08 et je suis passionné par la BD et l’animation depuis tout petit.'+
         'A mes heures perdues je m’adonne à la création de jeux vidéo. Bonne lecture !'+
@@ -304,7 +422,6 @@ $('#larryon').click(function(){
         '<p class="liste-serie"> #8 - One-Shot - <br />#10 - Chapitre 1 - <br /> #12 - Chapitre 2 - <br /> #14 - Chapitre 3 </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Larryon.png" alt="Larryon class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -320,6 +437,7 @@ $('#lao').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Lao.png" alt="Lao" class="img-presentation">'+
         '<p>Salut je suis Lao (et pas en bas) je suis Atsem en grande et petite section de maternelle.' +
         'Dans le webzine je suis l’auteure de Demon Spider. Fan de manga je suis influencée par mon manga' +
         'préféré Fullmetal Alchemist... ah et j’aime les framboises aussi !</p>'+
@@ -336,7 +454,6 @@ $('#lao').click(function(){
         '<p class="liste-serie"> #10 - One Shot <br/> #12 - Chapitre 1 </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Lao.png" alt="Lao" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -352,6 +469,7 @@ $('#midori').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Midori.png" alt="Midori" class="img-presentation">'+
         '<p>Hm...que dire de moi ? Et bien, à 20 ans je me lance pour la première fois dans la création d’une série originale' +
         'que vous aurez l’occasion de lire dans les pages du Kippon Dream ! Je dessine depuis que je suis petite mais' +
         'c’est au collège que j’ai sérieusement commencé à m’intéresser à la réalisation de mangas en faisant des doujinshis.' +
@@ -370,7 +488,6 @@ $('#midori').click(function(){
         '<p class="liste-serie"> #Dream Cup - One Shot <br/> #15 - Chapitre 1</p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Midori.png" alt="Midori" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -386,6 +503,7 @@ $('#oz').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Oz.png" alt="Oz" class="img-presentation">'+
         '<p>Yo les KippReaders,  moi c’est Mickael. Je sais pas trop quoi dire alors je vais faire court.' +
         'Comme vous j’ai été pris de passion pour le manga très jeune dans ma vie, avec des films comme' +
         ' Macross ou encore Oz-Teacher, d’où mon pseudo ! Mais le manga qui m’a réellement donné envie d’écrire' +
@@ -404,8 +522,7 @@ $('#oz').click(function(){
         '<p class="liste-serie"> #8 - One Shot <br/> #10 - Chapitre 1 <br/> #12 - Chapitre 2 <br/>' +
         '#14 - Chapitre 3 </p>'+
         '</div>'+
-        '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Oz.png" alt="Oz" class="img-presentation">'+
+        '<div class="col-md-6">' +
         '</div>'+
         '</div>'+
         '</div>'+
@@ -421,6 +538,7 @@ $('#smaiky').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/Smaiky.png" alt="Smaiky" class="img-presentation">'+
         '<p>Salut tout le monde, moi c’est Smaiky ! Comme vous pouvez vous en douter j’adore le dessin et ' +
         'plus particulièrement dessiner des planches de BD !! C’est un passe-temps que j’ai depuis tout petit' +
         ' et qui a évolué en passion, surtout lorsque j’ai découvert le style Manga ! Après avoir appris ce que' +
@@ -443,7 +561,6 @@ $('#smaiky').click(function(){
         ' <br/> #13 - Chapitre 4 Partie 2 <br/> #15 - Chapitre 5 </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/Smaiky.png" alt="Smaiky" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -459,6 +576,7 @@ $('#thibault').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/thibault.png" alt="Thibault Deboom" class="img-presentation">'+
         '<p>Hoy ! Moi c’est Thibault Deboom j’ai 18 ans, je dessine depuis toujours, ma principale source d’inspiration est Dragon Ball ,' +
         ' ça se ressent certainement dans mon style !</p>'+
         '</div>'+
@@ -474,7 +592,6 @@ $('#thibault').click(function(){
         '<p class="liste-serie"> #12 - One Shot <br/> #14 - Chapitre 1 </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/thibault.png" alt="Thibault Deboom" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -490,6 +607,7 @@ $('#unico').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/unico.png" alt="Unico" class="img-presentation">'+
         '<p>Hey lecteurs du Kippon Dream! Unico pour vous servir ! Je me présente : jeune apprentie « scientifique fou » ' +
         'de 17 ans qui s’est donné pour défi de changer le monde avec pour arme un crayon de papier et dessinatrice dans ' +
         'le Kippon Dream ! Je dessine depuis toujours et suis une grosse dévoreuse de BD, de manga et d’animé. J’aime '+
@@ -508,7 +626,6 @@ $('#unico').click(function(){
         '<p class="liste-serie"> #4 - One Shot </p>'+
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/unico.png" alt="Unico" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
@@ -524,6 +641,7 @@ $('#yumi').click(function(){
         '<div class="modal-body">'+
         '<div class="row">'+
         '<div>'+
+        '<img src="/assets/presentation/yumi.png" alt="Yumi" class="img-presentation">'+
         '<p>Salut tout le monde~! Je suis Yumi! Je viens de débarquer dans l’univers du Kippon Dream! Quelle aventure! ' +
         'Pour le moment, j’ai réalisé des illustrations : une avec Kowasu dans le numéro 15, et surtout  la couverture ' +
         'et la quatrième de couverture du numéro papier que vous tenez entre vos mains avec mon amie Twildette à la colo ! ' +
@@ -543,7 +661,6 @@ $('#yumi').click(function(){
         '<p>Projet en développement </p>' +
         '</div>'+
         '<div class="col-md-6">'+
-        '<img src="/assets/presentation/yumi.png" alt="Yumi" class="img-presentation">'+
         '</div>'+
         '</div>'+
         '</div>'+
