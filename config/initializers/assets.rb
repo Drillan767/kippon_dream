f5357be859edb92695b27292c792fe0042f9ea05
g4-cft <jd.levarato@gmail.com>
2016-08-26 17:46:55 +0200

    Nouvelles pages

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,8 @@ Rails.application.config.assets.precompile += %w( application.css )
 Rails.application.config.assets.precompile += %w( sd.css )
 Rails.application.config.assets.precompile += %w( hd.css )
 Rails.application.config.assets.precompile += %w( fontawesome.css )
+Rails.application.config.assets.precompile += %w( tablette.css )
+Rails.application.config.assets.precompile += %w( mobile.css )
 
 #JS
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
